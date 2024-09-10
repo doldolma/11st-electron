c98212cace677f15511c98cdcba3cb001df46d17
doldolma <hdy@doldolma.com>
2024-09-11 00:37:20 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ function BottomNav({value, setValue}) {
                 value={value}
                 onChange={handleChange}
             >
-                <BottomNavigationAction label="11번가"/>
+                <BottomNavigationAction label="11st"/>
                 <BottomNavigationAction label="데이터"/>
                 <BottomNavigationAction label="앱 설정"/>
             </BottomNavigation>
