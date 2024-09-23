c1d6b1abcaad90566369516781cc9f6f3cda8878
doldolma <hdy@doldolma.com>
2024-09-23 23:06:11 +0900

    11번가 소카테고리 페이지네이션 적용

--- src/util/eleven.js
+++ src/util/eleven.js
@@ -15,7 +15,115 @@ export default async function getCategoryProducts(category, updateStatus) {
 
     const isBigCategory = !category.url.includes('ctgr2No');
 
-    return (await findShootingBest(data.data, isBigCategory, updateStatus));
+    let items = [];
+    if (isBigCategory) {
+        items = await getBigCategoryProducts(category);
+    } else {
+        items = await getSmallCategoryProducts(category);
+    }
+
+    // 옵션들 가져오기
+    return await getAllOptions(items, updateStatus);
+
+}
+
+async function getBigCategoryProducts(category) {
+
+    let data = (await axios.get("https://apis.11st.co.kr/pui/v2/page?pageId=PCSHOOTING_CATE&" + category.url)).data;
+
+    if (data.resultCode !== 200) {
+        console.log("통신에러");
+        return;
+    }
+
+    let carrList = data.data;
+
+    let items = [];
+    let rank = 1;
+
+    for (const carr of carrList) {
+        // PC_ProductGrid_Basic 타입 찾기
+        let blockList = carr.blockList;
+        if (!blockList) continue;
+
+
+        for (const block of blockList) {
+            if (block.type !== findType) {
+                continue;
+            }
+
+            // products
+            let list = block.list;
+            if (!list) continue;
+
+            for (const listElement of list) {
+                let productsMeta = listElement.logData;
+                for (const item of listElement.items) {
+                    item.rank = rank;
+                    rank++;
+                }
+                if (productsMeta.area === 'shooting_best') {
+                    items = [...items, ...listElement.items];
+                }
+            }
+        }
+    }
+
+    return items;
+}
+
+// 소규모 카테고리.. 10페이지까지.. 중간에 아이템 없으면 멈추기
+async function getSmallCategoryProducts(category) {
+    let allItems = [];
+    let rank = 1;
+
+    for (let i = 1; i < 10; i++) {
+        let data = (await axios.get("https://apis.11st.co.kr/pui/v2/page?pageId=PCSHOOTING_CATE&" + category.url + "&pageNo=" + i)).data;
+
+        if (data.resultCode !== 200) {
+            console.log("통신에러");
+            return;
+        }
+        let carrList = data.data;
+
+        let items = [];
+
+        for (const carr of carrList) {
+            // PC_ProductGrid_Basic 타입 찾기
+            let blockList = carr.blockList;
+            if (!blockList) continue;
+
+
+            for (const block of blockList) {
+                if (block.type !== findType) {
+                    continue;
+                }
+
+                // products
+                let list = block.list;
+                if (!list) continue;
+
+                for (const listElement of list) {
+                    let productsMeta = listElement.logData;
+                    for (const item of listElement.items) {
+                        item.rank = rank;
+                        rank++;
+                    }
+                    if (productsMeta.area === 'productlist') {
+                        items = [...items, ...listElement.items];
+                    }
+                }
+            }
+        }
+
+        if (items.length === 0) {
+            // 그만하자
+            break;
+        } else {
+            allItems = [...allItems, ...items];
+        }
+    }
+    return allItems;
 }
 
 async function findShootingBest(carrList, isBigCategory, updateStatus) {
@@ -44,7 +152,7 @@ async function findShootingBest(carrList, isBigCategory, updateStatus) {
                     rank++;
                 }
                 if (isBigCategory) {
-                    if (productsMeta.area === 'shooting_best') {
+                    if (productsMeta.area === 'shooting_best' || productsMeta.area === 'productlist') {
                         items = [...items, ...listElement.items];
                     }
                 } else {
@@ -56,6 +164,40 @@ async function findShootingBest(carrList, isBigCategory, updateStatus) {
         }
     }
 
+    let totalItems = items.length;
+
+    let products = [];
+
+    // 옵션 가져오기
+    let a = 0;
+    for (const item of items) {
+        let progress = (++a / totalItems * 100).toFixed(1);
+        updateStatus(`진행중(${progress}%)`);
+
+        let newVar = await getOptions(item.prdNo);
+
+        if (!newVar) {
+            // 옵션이 없으니 현재 상품 정보 조회 해서 추가 하기
+            let product = await getProductInfo(item.prdNo);
+            if (product) {
+                product.rank = item.rank;
+                products.push(product);
+            }
+        } else {
+            for (const newVarElement of newVar) {
+                let product = await getProductInfo(newVarElement.productNo);
+                product.rank = item.rank;
+                if (product) {
+                    products.push(product);
+                }
+            }
+        }
+    }
+
+    return products;
+}
+
+async function getAllOptions(items, updateStatus) {
     let totalItems = items.length;
 
     let products = [];
